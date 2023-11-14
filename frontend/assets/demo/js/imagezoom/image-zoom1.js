386fa64a6fa4e5c11a425e561c3e7bd771220d22
Dipankar Biswas <themedesigner3@gmail.com>
2023-11-14 21:38:17 +0600

    update

---
+++ frontend/assets/demo/js/imagezoom/image-zoom1.js
@@ -0,0 +1,4 @@
+let product_img = document.querySelector('.image-zoom .image img');
+let lens = document.querySelector('.image-zoom .image .magnifier-lens');
+let magnified_img = document.querySelector('.image-zoom .details .magnified-img');
+
